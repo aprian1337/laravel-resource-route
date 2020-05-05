e6b357e6a91e37a2927f2c5c7a4ef9d24291d405
aprian1337 <dwikyapriyan@gmail.com>
2020-05-05 22:21:41 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}">
+
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/custom.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.css">
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 </head>
 
 <body>
@@ -52,131 +54,122 @@
             </div>
         </div>
     </nav>
-<header
-    <div class="container text-center">
+    <header <div class="container text-center">
         <p></p>
-    <div class="jumbotron" style="height: 400px; padding-top: 150px;">
+        <div class="jumbotron" style="height: 400px; padding-top: 150px;">
             <center>
                 <h2>W E L C O M E</h2>
             </center>
-         </div>
-     </div>
-</header>
+        </div>
+        </div>
+    </header>
 
-<!-- Footer -->
-<footer class="page-footer font-small indigo">
+    <!-- Footer -->
+    <footer class="page-footer font-small indigo">
 
-  <!-- Footer Links -->
-  <div class="container">
+        <!-- Footer Links -->
+        <div class="container">
 
-    <!-- Grid row-->
-    <div class="row text-center d-flex justify-content-center pt-5 mb-3">
+            <!-- Grid row-->
+            <div class="row text-center d-flex justify-content-center pt-5 mb-3">
 
-      <!-- Grid column -->
-      <!-- Grid column -->
-    </div>
-    <!-- Grid row-->
-    <hr class="rgba-white-light" style="margin: 0 15%;">
+                <!-- Grid column -->
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row-->
+            <hr class="rgba-white-light" style="margin: 0 15%;">
 
-    <!-- Grid row-->
-    <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+            <!-- Grid row-->
+            <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
 
-      <!-- Grid column -->
+                <!-- Grid column -->
 
-      <!-- Grid column -->
+                <!-- Grid column -->
 
-    </div>
-    <!-- Grid row-->
-    <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+            </div>
+            <!-- Grid row-->
+            <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
 
-    <!-- Grid row-->
-    <div class="row pb-3">
+            <!-- Grid row-->
+            <div class="row pb-3">
 
-      <!-- Grid column -->
-      <div class="col-md-12">
+                <!-- Grid column -->
+                <div class="col-md-12">
 
-        <div class="mb-5 flex-center">
+                    <div class="mb-5 flex-center">
+
+                    </div>
+
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row-->
 
         </div>
+        <!-- Footer Links -->
 
-      </div>
-      <!-- Grid column -->
+        <!-- Copyright -->
+        <footer class="footer-distributed">
 
-    </div>
-    <!-- Grid row-->
+            <div class="footer-left">
+                <h3>Aku<span>Rapopo's</span></h3>
 
-  </div>
-  <!-- Footer Links -->
+                <p class="footer-links">
+                    <a href="#">Product</a>
+                    |
+                    <a href="#">Customer</a>
+                    |
+                    <a href="#">Transaction</a>
+                </p>
 
-  <!-- Copyright -->
- <footer class="footer-distributed">
+                <p class="footer-company-name">© 2019 Akurapopo's</p>
+            </div>
 
-			<div class="footer-left">
-				<h3>Aku<span>Rapopo's</span></h3>
-
-				<p class="footer-links">
-					<a href="#">Product</a>
-					|
-					<a href="#">Customer</a>
-					|
-					<a href="#">Transaction</a>
-				</p>
-
-				<p class="footer-company-name">© 2019 Akurapopo's</p>
-			</div>
-
-			<div class="footer-center">
-				<div>
-					<i class="fa fa-map-marker"></i>
-					  <p>Kota Malang</p>
-				</div>
-
-				<div>
-					<i class="fa fa-phone"></i>
-					<p>+62 857-1209-2239</p>
-				</div>
-				<div>
-					<i class="fa fa-envelope"></i>
-					<p><a href="#">support@akurapopo.com</a></p>
-				</div>
-			</div>
-			<div class="footer-right">
-				<p class="footer-company-about">
-					<span>About the company</span>
-					Akurapopo's menjual berbagai produk dengan harga murah, kualiatas terjamin dan pelayanan ramah.</p>
-				<div class="social-icons">
-                      <a class="social-icon social-icon--twitter" data-toggle="tooltip" data-placement="bottom" title="Twitter">
-                        <i class="fa fa-twitter"></i>
-                      </a>
-                      <a class="social-icon social-icon--instagram" data-toggle="tooltip" data-placement="bottom" title="Instagram">
-                        <i class="fa fa-instagram"></i>
-                      </a>
-                      <a class="social-icon social-icon--facebook" data-toggle="tooltip" data-placement="bottom" title="Facebook">
-                        <i class="fa fa-facebook"></i>
-                      </a>
+            <div class="footer-center">
+                <div>
+                    <i class="fa fa-map-marker"></i>
+                    <p>Kota Malang</p>
                 </div>
-			</div>
-		</footer>
-  <!-- Copyright -->
 
-</footer>
-<!-- Footer -->
+                <div>
+                    <i class="fa fa-phone"></i>
+                    <p>+62 857-1209-2239</p>
+                </div>
+                <div>
+                    <i class="fa fa-envelope"></i>
+                    <p><a href="#">support@akurapopo.com</a></p>
+                </div>
+            </div>
+            <div class="footer-right">
+                <p class="footer-company-about">
+                    <span>About the company</span>
+                    Akurapopo's menjual berbagai produk dengan harga murah, kualiatas terjamin dan pelayanan ramah.</p>
+                <div class="social-icons">
+                    <a class="social-icon social-icon--twitter" data-toggle="tooltip" data-placement="bottom"
+                        title="Twitter">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a class="social-icon social-icon--instagram" data-toggle="tooltip" data-placement="bottom"
+                        title="Instagram">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                    <a class="social-icon social-icon--facebook" data-toggle="tooltip" data-placement="bottom"
+                        title="Facebook">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                </div>
+            </div>
+        </footer>
+        <!-- Copyright -->
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="{{asset('vendor/jquery/jquery-3.2.1.min.js')}}"></script>
-    <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="assets/js/jquery.js"></script> 
-    <script src="assets/js/popper.js"></script> 
-    <script src="assets/js/bootstrap.js"></script>
+    </footer>
+    <!-- Footer -->
+
     <script>
-        $(document).ready(function(){
-            $('[data-toggle="tooltip"]').tooltip();   
-            });
+        $(document).ready(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
     </script>
 </body>
 
